b9cf81aa423024a462f85f1dbf82e8cba3783176
snpro <sna.project26@gmail.com>
2026-05-18 08:55:28 +0530

    fix: setup for local

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://sn-edu.vercel.app", //"http://localhost:3000",
+    origin: "http://localhost:3000", //"https://sn-edu.vercel.app",
     credentials: true,
   }),
 );
